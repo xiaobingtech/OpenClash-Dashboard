a9174b5b0535ecf509df25a01bbd1b59f38a22a2
bin64 <yanmou1996@gmail.com>
2024-11-19 11:12:39 +0800

    Initial Commit

---
+++ OpenClash Dashboard/OpenClash_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClash_DashboardApp.swift
+//  OpenClash Dashboard
+//
+//  Created by Mou Yan on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenClash_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenClash DashboardTests/OpenClash_DashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClash_DashboardTests.swift
+//  OpenClash DashboardTests
+//
+//  Created by Mou Yan on 11/19/24.
+//
+
+import Testing
+@testable import OpenClash_Dashboard
+
+struct OpenClash_DashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
